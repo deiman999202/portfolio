55afcad90edebbc5a975813d02612335873942f5
deiman999202 <deiman999202@gmail.com>
2023-10-09 13:42:28 +0200

    fifth commit

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,7 +1,7 @@
 
 const AboutPage = () => {
   return (
-    <section className='w-12/12 h-screen flex justify-center mt-4 md:mt-32'>
+    <section className='w-12/12 h-screen flex justify-center mt-4 md:mt-32 bg-yellow-400'>
         <p className="w-11/12 md:w-8/12 mb-4 bg-white p-8 h-max text-md sm:text-xl overflow-auto">An innovative and forward-thinking <b>Junior Frontend Developer</b>, I possess the requisite passion and skills to make a significant impact in the world of web development. 
             <br />With a strong focus on the user experience, I have meticulously sharpened my skills and applied them in <b>3 full-stack pet projects</b>, enhancing the understanding of the development lifecycle from concept to completion. My expertise includes proficiency in <b>HTML, CSS, JavaScript</b>, responsive design, and an array of cutting-edge frameworks and libraries.
             <br />I am not just technologically inclined, but I also foster <b>creativity, problem-solving, and team collaboration</b>. My projects testify to my dedication and commitment to building innovative, functional, and aesthetically pleasing websites and applications.
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,7 +3,7 @@ import Header from '../components/Header'
 
 const MainPage = () => {
   return (
-    <main className='bg-yellow-400 h-120 md:h-screen'>
+    <main className='bg-yellow-400 h-screen'>
         <Header />
         <Outlet />
     </main>
